54326a2f56d2eea9521d22e06441b459b956eec6
Denis <denis.panggabean@accenture.com>
2024-03-25 16:40:33 +0700

    add custom rule

--- src/main/kotlin/org/example/detekt/MyRule.kt
+++ src/main/kotlin/org/example/detekt/MyRule.kt
@@ -8,20 +8,30 @@ import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
 import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtProperty
 
-class MyRule(config: Config) : Rule(config) {
+class EnumValueSnakeCaseNaming(config: Config = Config.empty) : Rule(config) {
+
     override val issue = Issue(
         javaClass.simpleName,
-        Severity.CodeSmell,
-        "Custom Rule",
-        Debt.FIVE_MINS,
+        Severity.Style,
+        "Enum value property should be named using snake_case format.",
+        Debt.FIVE_MINS
     )
 
-    override fun visitClass(klass: KtClass) {
-        super.visitClass(klass)
-
-        if (klass.isInner()) {
-            report(CodeSmell(issue, Entity.atName(klass), "Custom message"))
+    override fun visitProperty(property: KtProperty) {
+        super.visitProperty(property)
+        if (property.isEnumValueProperty() && property.name?.isSnakeCase() == false) {
+            report(CodeSmell(issue, Entity.from(element = property.parent), "Custom Message"))
         }
+    }
+
+    private fun KtProperty.isEnumValueProperty(): Boolean {
+        val parentEnum = parent.children.find { it is KtClass } as? KtClass
+        return parentEnum?.isEnum() == true && parentEnum.isValue() && hasInitializer()
+    }
+
+    private fun String.isSnakeCase(): Boolean {
+        return this.matches(Regex("[a-z]+(_[a-z]+)*"))
     }
 }
